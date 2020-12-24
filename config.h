8ac4a5b7131bdb4771c82dbbb3941a65f2ad709b
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2020-12-24 16:01:10 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -4,15 +4,11 @@
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"Mononoki Nerd Font:size=15:antialias=true:autohint=true","JoyPixels:size=13:antialias=true:autohint=true"};
+static const char *fonts[]          = {"Mononoki Nerd Font:size=14:antialias=true:autohint=true","JoyPixels:size=13:antialias=true:autohint=true"};
 										
-static const char dmenufont[]       = "Monospace:size=16";
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -27,12 +23,12 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
 	/*"dwmblocks", NULL,*/
-	/*"dwmbar",  NULL,*/
+	/*"slstatus", NULL,*/
 	"/home/phil/dwm-bar/dwm_bar.sh", NULL,
 	"xcompmgr", NULL,
 	"lxpolkit", NULL,
 	"/home/phil/dwm-6.2d/testwallpaper", NULL,
-	"sxhkd", "-c", "/home/phil/dwm-6.2d/sxhkdrc", NULL, 
+	"sxhkd", "-c", "/home/phil/dwm-6.2d/sxhkdrc", NULL,
 	NULL /* terminate */
 };
 
@@ -74,8 +70,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", "Noto Sans-15", NULL };
-/*{ "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "termite", NULL };
 
 static Key keys[] = {
